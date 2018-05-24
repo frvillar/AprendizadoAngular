471c51d5e3cdd428e6f79d0a10ba868a5e9682dc
frvillar <villar@casavillar.com.br>
2018-05-24 12:23:12 -0300

    Início directives

--- angularcurso1/prj-basics-final/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ angularcurso1/prj-basics-final/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter, Input, ViewChild, ElementRef } from '@angular/core';
+import { Ingredient } from '../../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -6,10 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-edit.component.css']
 })
 export class ShoppingEditComponent implements OnInit {
+  @Output() newIngredient= new EventEmitter<Ingredient>();
+  @ViewChild('nameInput') name: ElementRef;
+  @ViewChild('amountInput') amount: ElementRef;
 
   constructor() { }
 
   ngOnInit() {
   }
+
+  onAddClick(){
+    const ing: Ingredient  = new Ingredient(this.name.nativeElement.value, this.amount.nativeElement.value)
+    this.newIngredient.emit(ing);
+  }
+
 
 }
